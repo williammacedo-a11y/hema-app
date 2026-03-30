4ce2cfd854468e9ad5637fd5f7a0756ccb94e249
William Rutyna Macedo <11807240975@gpinho.intranet>
2026-03-30 10:16:23 -0300

    fix docker file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 
   const logger = new Logger('Bootstrap');
   logger.log(`✅ Server iniciado e escutando na porta ${port}`);
